381f30f3f7fb3f3428e6868cab37df364861366b
Abhishek Negi <abhishek.negi@rapidsoft.co.in>
2025-09-27 13:01:58 +0530

    routes issue fixes

--- src/utilty/PublicRoute.jsx
+++ src/utilty/PublicRoute.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PublicRoute = () => {
-  const token = localStorage.getItem("token"); 
-  return !token ? <Navigate to="/" /> : <Outlet />;
+  const token = localStorage.getItem("token");
+
+  return token ? <Navigate to="/dashboard" /> : <Outlet />;
 };
 
 export default PublicRoute;
